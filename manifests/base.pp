77d0afac4967cfcbe347d3a57654e0dee98c9fe5
Adrian Webb <adrian.webb@venturiscm.com>
2012-05-22 20:11:29 -0400

    Adding a base / default server node.

---
+++ manifests/base.pp
@@ -0,0 +1,48 @@
+
+node default {
+
+  include params
+
+  #-----------------------------------------------------------------------------
+
+  class { 'server': }
+  class { 'puppet': module_paths => [ "${params::puppet_path}/modules" ] }
+
+  class { 'ntp': autoupdate => false }
+  class { 'locales':
+    locales => [ $params::locale ],
+  }
+
+  class { 'iptables': allow_icmp => $params::allow_icmp }
+
+  #---
+
+  class { 'ssh': port => $params::ssh_port, user_groups => [ $params::admin_group ] }
+  class { 'sudo': permissions => [
+    "%${params::admin_group} ALL=(ALL) ALL",
+  ] }
+
+  class { 'users':
+    user_names  => [ $params::admin_name ],
+    user_groups => [ $params::admin_group ],
+    key         => $params::key,
+    email       => $params::admin_email,
+    editor      => $params::admin_editor,
+    umask       => $params::user_umask,
+    init        => true,
+  }
+
+  #---
+
+  class { 'git':
+    git_home   => $params::git_home,
+    git_groups => [ 'git', $params::admin_group ],
+    key        => $params::key,
+    root_email => $params::git_root_email,
+    skel_email => $params::git_skel_email,
+  }
+
+  git::repo { $params::puppet_repo:
+    git_home => $params::git_home,
+  }
+}
