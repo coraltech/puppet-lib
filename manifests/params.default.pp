8a33087c14a2e2a496fab2ee75b37bef80755c94
Adrian Webb <adrian.webb@venturiscm.com>
2012-05-22 20:10:29 -0400

    Adding an initial set of base parameters for our server nodes.

---
+++ manifests/params.default.pp
@@ -0,0 +1,31 @@
+
+class params_default {
+
+  #-----------------------------------------------------------------------------
+  # To use:
+  #
+  # 1. Copy this file to "manifests/params.pp"
+  # 2. Rename this class to "params".
+  # 3. Fill in your values, particularly any values with a <VALUE> format below.
+
+  $locale         = 'en_US.UTF-8 UTF-8'
+
+  $ssh_port       = 22
+  $allow_icmp     = true
+
+  $user_umask     = 002
+
+  $admin_name     = 'admin'
+  $admin_group    = $admin_name
+  $admin_editor   = 'vim'
+  $admin_email    = ''
+
+  $git_home       = '/var/git'
+  $git_root_email = '<ROOT EMAIL HERE>'
+  $git_skel_email = '<DEFAULT USER EMAIL HERE>'
+
+  $puppet_repo    = 'puppet.git'
+  $puppet_path    = "${git_home}/${puppet_repo}"
+
+  $key            = '<PUBLIC KEY HERE>'
+}
