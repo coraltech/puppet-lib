80f33579edb641f4f35b288f722f063e571c696d
Adrian Webb <adrian.webb@venturiscm.com>
2012-06-05 03:18:18 -0400

    Adding the Percona database service.

--- manifests/db.pp
+++ manifests/db.pp
@@ -3,4 +3,13 @@ import 'base.pp'
 
 node db inherits base {
 
+  include params
+
+  #-----------------------------------------------------------------------------
+  # MySQL(ish) database
+
+  class { 'percona':
+    server          => true,
+    percona_version => '5.5',
+  }
 }
