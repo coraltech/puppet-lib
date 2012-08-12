5379708881305549ce8b4a6c911f2fb5c65b7f1c
Adrian Webb <adrian.webb@venturiscm.com>
2012-08-11 22:03:24 -0400

    Updating the file_server profile with parameter and requirement changes.

--- profiles/file_server.pp
+++ profiles/file_server.pp
@@ -22,9 +22,7 @@ class file_server inherits base {
   # Environment
 
   if ! empty($servers) {
-    file_server::server { $servers:
-      require => Class['apache'],
-    }
+    file_server::server { $servers: }
   }
 }
 
@@ -43,7 +41,6 @@ define file_server::server ( $server = $name ) {
     auth_url      => hiera("file_server_${server}_cloudfuse_auth_url", $cloudfuse::params::auth_url),
     cloud_user    => hiera("file_server_${server}_cloudfuse_cloud_user", $cloudfuse::params::cloud_user),
     cloud_api_key => hiera("file_server_${server}_cloudfuse_cloud_api_key", $cloudfuse::params::cloud_api_key),
-    container     => hiera("file_server_${server}_cloudfuse_container", $cloudfuse::params::container),
     cache_timeout => hiera("file_server_${server}_cloudfuse_cache_timeout", $cloudfuse::params::cache_timeout),
     gid           => hiera("file_server_${server}_cloudfuse_gid", $cloudfuse::params::gid),
     umask         => hiera("file_server_${server}_cloudfuse_umask", $cloudfuse::params::umask),
@@ -51,6 +48,7 @@ define file_server::server ( $server = $name ) {
 
   apache::vhost::file { $server:
     doc_root            => $mount_dir,
+    server_name         => hiera("file_server_${server}_apache_server_name", $apache::params::server_name),
     aliases             => hiera("file_server_${server}_apache_aliases", $apache::params::aliases),
     admin_email         => hiera("file_server_${server}_apache_admin_email", $apache::params::admin_email),
     vhost_ip            => hiera("file_server_${server}_apache_vhost_ip", $apache::params::vhost_ip),
@@ -63,7 +61,6 @@ define file_server::server ( $server = $name ) {
     ssl_key             => hiera("file_server_${server}_apache_ssl_key", $apache::params::ssl_key),
     error_log_level     => hiera("file_server_${server}_apache_error_log_level", $apache::params::error_log_level),
     rewrite_log_level   => hiera("file_server_${server}_apache_rewrite_log_level", $apache::params::rewrite_log_level),
-    extra               => hiera("file_server_${server}_apache_extra", {}),
     require             => Cloudfuse::Mount[$server],
   }
 }
