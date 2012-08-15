e3feb5041af43ad99e27458dbb2167df679ee58d
Adrian Webb <adrian.webb@venturiscm.com>
2012-08-15 04:30:43 -0400

    Updating the percona_server profile with parameter lookup changes.

--- profiles/percona_server.pp
+++ profiles/percona_server.pp
@@ -6,7 +6,7 @@ class percona_server inherits base {
   #-----------------------------------------------------------------------------
   # Configurations
 
-  $databases = hiera('percona_server_databases', [])
+  $databases = global_array('percona_server_databases', [])
 
   #-----------------------------------------------------------------------------
   # Required systems
@@ -29,7 +29,7 @@ class percona_server inherits base {
 
 define percona_server::database ( $database = $name ) {
 
-  $databases = hiera("percona_server_database_${database}_databases", [])
+  $databases = global_array("percona_server_database_${database}_databases", [])
 
   if ! empty($databases) {
     $database_real = $databases
@@ -39,12 +39,12 @@ define percona_server::database ( $database = $name ) {
   }
 
   percona::database { $database_real:
-    ensure        => hiera("percona_server_database_${database}_ensure", 'present'),
-    sql_dump_file => hiera("percona_server_database_${database}_sql_dump_file", $percona::params::database_sql_dump_file),
-    user_name     => hiera("percona_server_database_${database}_user", $percona::params::user_name),
-    password      => hiera("percona_server_database_${database}_password", $percona::params::user_password),
-    permissions   => hiera("percona_server_database_${database}_permissions", $percona::params::user_permissions),
-    grant         => hiera("percona_server_database_${database}_grant", $percona::params::user_grant),
-    remote        => hiera("percona_server_database_${database}_allow_remote", $percona::params::allow_remote),
+    ensure        => global_param("percona_server_database_${database}_ensure", 'present'),
+    sql_dump_file => global_param("percona_server_database_${database}_sql_dump_file", $percona::params::database_sql_dump_file),
+    user_name     => global_param("percona_server_database_${database}_user", $percona::params::user_name),
+    password      => global_param("percona_server_database_${database}_password", $percona::params::user_password),
+    permissions   => global_param("percona_server_database_${database}_permissions", $percona::params::user_permissions),
+    grant         => global_param("percona_server_database_${database}_grant", $percona::params::user_grant),
+    remote        => global_param("percona_server_database_${database}_allow_remote", $percona::params::allow_remote),
   }
 }
